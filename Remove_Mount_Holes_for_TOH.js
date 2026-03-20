0cd3a92e63e0dc85705eb74c40c189d10b048277
Patrick Tewell <48372125+patrick-tewell@users.noreply.github.com>
2026-03-20 11:09:59 -0400

    Rename and update to non-function structure

--- Remove_Mount_Holes_for_TOH.js
+++ Remove_Mount_Holes_for_TOH.js
@@ -1,27 +1,23 @@
-﻿function fnRemove_Mount_Holes_for_TOH()
-{
-
-// UCS converted to JS 02-25-26 16:59:01
-//Modifies Drawer Front
-
-//*** Applies to each DOR OBJ_PART ***
+﻿//*** Applies to each DOR OBJ_PART ***
 
 // Will check for any closet doors that have bottom hinges and delete the hinge plate mounting holes for that door
 // This could cause issues if we ever make bottom hinge doors that don't have a hamper and need those holes
 
+if (_cab.CLASS != ASM_CLASS_CLOSET) {
+	return;
+}
 
-if (_cab.CLASS != ASM_CLASS_CLOSET) return;
+let opening = _this.GetParent();
 
-var opening = _this.GetParent();
-
-if (!opening) return;
-if (opening.NAME != 'DOR_OPEN')	return;
+if (!opening || opening.NAME != 'DOR_OPEN') {
+	return;
+}
 
 if (opening.GetParameterValue('HNG') == 4) {
-	var plate = _this.GetNextSibling();
+	let plate = _this.GetNextSibling();
 	while (plate) {
 		if (plate.NAME == 'S_HNGPLT') {
-			var hole = plate.GetFirstChild();
+			let hole = plate.GetFirstChild();
 			while (hole) {
 				if (hole.NAME == '_HPVBORE') {
 					hole.SetParameter('QTY', 0);
@@ -31,7 +27,4 @@ if (opening.GetParameterValue('HNG') == 4) {
 		}
 		plate = plate.GetNextSibling();
 	}
-}
-
-
 }

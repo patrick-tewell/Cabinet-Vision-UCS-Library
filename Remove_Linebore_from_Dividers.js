409eebc31779c7a33e27d8b6ea66d2de67fb4bbc
Patrick Tewell <48372125+patrick-tewell@users.noreply.github.com>
2026-03-20 11:14:15 -0400

    Update var declarations

--- Remove_Linebore_from_Dividers.js
+++ Remove_Linebore_from_Dividers.js
@@ -5,7 +5,7 @@ if (_cab.CLASS != ASM_CLASS_CLOSET) {
 }
 
 function findBore(targetName, targetFace) {
-    var node = _this.GetFirstChild();
+    let node = _this.GetFirstChild();
     while (node) {
         if (node.NAME === targetName && node.GetParameterValue('_FACEWP') === targetFace) {
             return node;
@@ -18,18 +18,18 @@ function findBore(targetName, targetFace) {
 function adjustBore(bore) {
     if (!bore) return;
 
-    var rept = bore.GetParameterValue('REPT');
-    var y    = bore.GetParameterValue('Y');
-    var sp   = bore.GetParameterValue('SPCNG');
+    let rept = bore.GetParameterValue('REPT');
+    let y    = bore.GetParameterValue('Y');
+    let sp   = bore.GetParameterValue('SPCNG');
 
     if (typeof rept === 'number') bore.SetParameter('REPT', rept - 2);
     if (typeof y === 'number' && typeof sp === 'number') bore.SetParameter('Y', y + sp);
 }
 
-var fl = findBore('LFVBORE', 1); // Front Left
-var rl = findBore('LRVBORE', 1); // Rear Left
-var fr = findBore('LFVBORE', 2); // Front Right
-var rr = findBore('LRVBORE', 2); // Rear Right
+let fl = findBore('LFVBORE', 1); // Front Left
+let rl = findBore('LRVBORE', 1); // Rear Left
+let fr = findBore('LFVBORE', 2); // Front Right
+let rr = findBore('LRVBORE', 2); // Rear Right
 
 adjustBore(fl);
 adjustBore(rl);

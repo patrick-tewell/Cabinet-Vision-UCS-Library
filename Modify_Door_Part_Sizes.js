a987224b2009644a130c826a3a34286f434a05c4
Patrick Tewell <48372125+patrick-tewell@users.noreply.github.com>
2026-04-08 14:53:58 -0400

    Update and rename fnModify_Door_Part_Sizes.js to Modify_Door_Part_Sizes.js

--- Modify_Door_Part_Sizes.js
+++ Modify_Door_Part_Sizes.js
@@ -1,9 +1,3 @@
-﻿function fnModify_Door_Part_Sizes()
-{
-
-// UCS converted to JS 10-23-25 14:31:35
-//Modifies Door Top Rail Length
-
 //*** Applies to each DTR|DBR|DMR|DFP OBJ_PART ***
 
 if (_this.NAME == 'DTR' || _this.NAME == 'DBR' || _this.NAME == 'DMR') {
@@ -12,5 +6,4 @@ if (_this.NAME == 'DTR' || _this.NAME == 'DBR' || _this.NAME == 'DMR') {
 else {    // _this.NAME == 'DFP'
 	_this.DX -= 0.125;
 	_this.DY -= 0.125;
-}
 }

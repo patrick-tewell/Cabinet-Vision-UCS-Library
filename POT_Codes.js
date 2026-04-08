7c5693f38a4cf0d29d89be2ee2fa13c2853add24
Patrick Tewell <48372125+patrick-tewell@users.noreply.github.com>
2026-04-08 14:55:12 -0400

    Update and rename fnPOT_Codes.js to POT_Codes.js

--- POT_Codes.js
+++ POT_Codes.js
@@ -1,16 +1,6 @@
-﻿function fnPOT_Codes()
-{
-
-// UCS converted to JS 02-26-26 10:26:10
-//Modifies Top
-
 //*** Applies to each ROS|ROBK OBJ_PART ***
 
-
-// D&D spacing: 0.5" - 2"
-var potCode = 'CV0500';
-
-
+// potCode is a public variable found in the editor
 
 if (_cab.CLASS != ASM_CLASS_CLOSET) {
 	return;
@@ -18,5 +8,4 @@ if (_cab.CLASS != ASM_CLASS_CLOSET) {
 
 if (_this.NAME == 'ROS' || _this.NAME == 'ROBK') {
 	_this.COMMENT = potCode;
-}
 }
